8ea47a3076fdf1cd1e44dfcdf1c5b0ae7287a063
Viktor Gorokhov <patronusv@gmail.com>
2020-11-03 21:00:04 +0200

    соединяет верхний и нижний слайдеры

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
     $('.slider-big').slick({
         arrows: true,
-        slidesToShow:1,
+        slidesToShow: 1,
+        // fade: true
+        asNavFor: ".slider"
+        
     });
     $('.slider').slick({
         arrows: false,
-        slidesToShow:3,
+        slidesToShow: 3,
+        asNavFor: ".slider-big",        
+        mobileFirst: true,
+        focusOnSelect: true,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 7,
+                }
+            }
+        ]
     });
+
+    $('.slider').slick('setPosition');
     
 });
